8968b1fcc430dc637c522126f73dd288b77fcb60
Michael Leaney <mleaney@tokbox.com>
2015-09-11 11:49:26 +1000

    Clean up project

---
+++ README.md
@@ -0,0 +1,22 @@
+ # file-signature
+
+ Get file information based on file signature.  This utility is used to get file
+ information based on the file signature. It does so by looking at "magic numbers"
+ common formats contain.
+
+ Data for this script was generated from:
+ http://en.wikipedia.org/wiki/List_of_file_signatures
+
+ ## Example use:
+ ```js
+
+var fileSignature = require('file-signature');
+console.log(fileSignature.identify('path/to/file.jpg'));
+
+// outputs:
+// {
+//   extension: 'jpg',
+//   description: 'A commonly used method of lossy compression for digital photography (image).',
+//   mimeType: 'image/jpg'
+// }
+```
--- spec/file-signature.spec.js
+++ spec/file-signature.spec.js
@@ -2,15 +2,15 @@ var fileSignature = require('../file-signature.js');
 
 describe('File signatures', function() {
   it('should get correct mime type for gif', function() {
-    var info = fileSignature.identify('specs/example.gif');
+    var info = fileSignature.identify('spec/example.gif');
     expect(info.mimeType).toEqual('image/gif');
   });
   it('should get correct mime type for jpg', function() {
-    var info = fileSignature.identify('specs/example.jpg');
+    var info = fileSignature.identify('spec/example.jpg');
     expect(info.mimeType).toEqual('image/jpg');
   });
   it('should get correct mime type for png', function() {
-    var info = fileSignature.identify('specs/example.png');
+    var info = fileSignature.identify('spec/example.png');
     expect(info.mimeType).toEqual('image/png');
   });
 })
